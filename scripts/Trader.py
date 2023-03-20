5350a043cc6f07b6504880c2f7e46b9a088e4b99
schen7239 <schen7239@gmail.com>
2023-03-20 07:54:52 -0700

    Steve: Printing order books to see buy and sell liquidity

--- scripts/Trader.py
+++ scripts/Trader.py
@@ -1,15 +1,19 @@
 from typing import Dict, List
 # FOR SUBMISSION USE
-# from datamodel import OrderDepth, TradingState, Order
-from ..classes.datamodel import OrderDepth, TradingState, Order
+from datamodel import OrderDepth, TradingState, Order
+
+# from ..classes.datamodel import OrderDepth, TradingState, Order
 
 
 class Trader:
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
-		"""
-		Takes all buy and sell orders for all symbols as an input,
-		and outputs a list of orders to be sent
-		"""
+        
         result = {}
+        for product in state.order_depths.keys():
+            order_depth: OrderDepth = state.order_depths[product]
+            print(f'Product: {product}')
+            print(order_depth.buy_orders)
+            print(order_depth.sell_orders)
+            print("\n\n")
         return result
