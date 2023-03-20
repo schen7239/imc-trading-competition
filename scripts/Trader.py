b1a81e0b88225bb60451fcc04c1eac7eb41d9748
schen7239 <schen7239@gmail.com>
2023-03-20 16:19:54 -0700

    Steve: Created basic algo that trades to maintain balance between buy and sell order volumes at key liquidity levels

--- scripts/Trader.py
+++ scripts/Trader.py
@@ -8,12 +8,24 @@ from datamodel import OrderDepth, TradingState, Order
 class Trader:
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
-        
+        positions = state.position
         result = {}
         for product in state.order_depths.keys():
-            order_depth: OrderDepth = state.order_depths[product]
-            print(f'Product: {product}')
-            print(order_depth.buy_orders)
-            print(order_depth.sell_orders)
-            print("\n\n")
+            position = positions.get(product, 0)
+            buy_liquidity = max(state.order_depths[product].buy_orders)
+            sell_liquidity = min(state.order_depths[product].sell_orders)
+            buy_volume = sum(state.order_depths[product].buy_orders.values())
+            sell_volume = sum(state.order_depths[product].sell_orders.values())
+            
+            trade_volume = buy_volume + sell_volume + position
+            if buy_volume + sell_volume == 0:
+                continue
+            elif trade_volume == 0:
+                continue
+            elif trade_volume > 0:
+                result[product] = [Order(product, -trade_volume, sell_liquidity)]
+            else:
+                result[product] = [Order(product, trade_volume, buy_liquidity)]
+            
+            
         return result
